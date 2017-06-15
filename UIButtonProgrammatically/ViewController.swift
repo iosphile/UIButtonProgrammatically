2f6c36c7ea7994e4926f8d2c817e87e6ef149035
Rajesh Kommana <rajeshkommana@rajeshs-macbook-pro.local>
2017-06-15 16:09:03 +0800

    Initial Commit

---
+++ UIButtonProgrammatically/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIButtonProgrammatically
+//
+//  Created by Rajesh Kommana on 15/6/17.
+//  Copyright © 2017 Rajesh Kommana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
